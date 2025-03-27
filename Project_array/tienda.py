5e1b199882b7f0592c2501ffa4be3fcefe325548
miguelcamach0 <misacamachorangel@gmail.com>
2025-03-27 11:08:12 -0500

    menu-2
    Se hizo el menú

---
+++ Project_array/tienda.py
@@ -0,0 +1,12 @@
+def hacer_menu():
+    print("1. Registrar Nuevo Cliente.")
+    print("2. Eliminar Cliente.")
+    print("0. Salir del sistema.")
+    print("...seleccione una opción...")
+    opcion = int(input())
+    return  opcion
+
+
+
+# ****Zona de Código Principal ****
+aux_opcion = hacer_menu()
